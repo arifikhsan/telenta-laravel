5b0c69d1b761786b7a30dab7b64b1b97878254c8
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2025-08-06 08:58:56 +0700

    update description

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -40,7 +40,7 @@ import { Head, Link } from '@inertiajs/vue3';
                 >
                     <h1 class="mb-1 font-medium">Telenta</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
-                        Laravel has an incredibly rich ecosystem. <br />We suggest starting with the following.
+                        Aplikasi buat mencatat kandidat.
                     </p>
                     <ul class="mb-4 flex flex-col lg:mb-6">
                         <li
