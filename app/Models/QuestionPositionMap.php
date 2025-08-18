f05ee1f41b71adbd7de3308e47120c9bee86954e
tengku_fs_putra_x <tengku_fs_putra_x@telkomsel.co.id>
2025-08-18 13:46:38 +0700

    adding migrate and seeder

---
+++ app/Models/QuestionPositionMap.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionPositionMap extends Model
+{
+     public function position() {
+        return $this->belongsTo(Position::class);
+    }
+
+    public function question() {
+        return $this->belongsTo(Question::class);
+    }
+}
