40c6276ac308e6b8d29624498774dac4f974bdbc
tengku_fs_putra_x <tengku_fs_putra_x@telkomsel.co.id>
2025-09-01 20:01:32 +0700

    migration question

--- database/migrations/2025_08_17_130029_question.php
+++ database/migrations/2025_08_17_130029_question.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->string('question');
-            $table->string('status');
+            $table->text('question');
+            $table->string('status', length: 10);
             $table->timestamps();
         });
     }
