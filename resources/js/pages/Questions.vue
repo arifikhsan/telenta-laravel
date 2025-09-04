68725d1d4d6d7c9ee2134268c2e4254c539135c4
tengku_fs_putra_x <tengku_fs_putra_x@telkomsel.co.id>
2025-09-04 21:39:05 +0700

    feature interview

--- resources/js/pages/Questions.vue
+++ resources/js/pages/Questions.vue
@@ -11,27 +11,27 @@ defineProps({
   questions: {
     type: Array as () => QuestionEntity[], // Specify the type of roles as an array of Role objects
     required: true,
-  },
-});
+    },
+    });
 
 const columnHelper = createColumnHelper<QuestionEntity>();
 
 const columns: ColumnDef<QuestionEntity, any>[] = [
-  columnHelper.accessor('id', {
-    header: 'Id',
-    cell: ({ row }) => h('div', row.getValue('id')),
+columnHelper.accessor('id', {
+  header: 'Id',
+  cell: ({ row }) => h('div', row.getValue('id')),
   }),
-  columnHelper.accessor('question', {
-    header: 'Question',
-    cell: ({ row }) => h('div', { class: 'capitalize' }, row.getValue('question')),
+columnHelper.accessor('question', {
+  header: 'Question',
+  cell: ({ row }) => h('div', { class: 'capitalize' }, row.getValue('question')),
   }),
-  columnHelper.accessor('created_at', {
-    header: 'Created At',
-    cell: ({ row }) => h('div', formatStandardDate(row.getValue('created_at'))),
+columnHelper.accessor('created_at', {
+  header: 'Created At',
+  cell: ({ row }) => h('div', formatStandardDate(row.getValue('created_at'))),
   }),
-  columnHelper.accessor('updated_at', {
-    header: 'Updated At',
-    cell: ({ row }) => h('div', formatStandardDate(row.getValue('updated_at'))),
+columnHelper.accessor('updated_at', {
+  header: 'Updated At',
+  cell: ({ row }) => h('div', formatStandardDate(row.getValue('updated_at'))),
   }),
 ];
 </script>
@@ -41,7 +41,12 @@ const columns: ColumnDef<QuestionEntity, any>[] = [
 
   <AppLayout>
     <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-      <DataTable title="Question" description="Interview Question" :columns="columns" :data="questions" />
+      <div class="flex justify-end">
+        <Link :href="route('dashboard.candidates.create')">
+        <Button variant="default">Tambah Pertanyaan Baru</Button>
+      </Link>
     </div>
-  </AppLayout>
+    <DataTable title="Question" description="Interview Question" :columns="columns" :data="questions" />
+  </div>
+</AppLayout>
 </template>
